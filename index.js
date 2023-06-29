b2b08b4e4758aa4483875a3e744a7b1c11e1038e
Strawuzel <137295274+Strawuzel@users.noreply.github.com>
2023-06-29 20:56:00 +0200

    added img elements

---
+++ index.js
@@ -0,0 +1,12 @@
+const austria1444ImgEl = document.getElementById("austria-1444-img");
+const ottoman1444ImgEl =document.getElementById("ottoman-1444-img");
+const poland1444ImgEl = document.getElementById("poland-1444-img");
+const brandenburg1444ImgEl = document.getElementById("brandenburg-1444-img");
+const denmark1444ImgEl = document.getElementById("denmark-1444-img");
+const england1444ImgEl = document.getElementById("england-1444-img");
+const france1444ImgEl = document.getElementById("france-1444-img");
+const castille1444ImgEl = document.getElementById("castille-1444-img");
+const muscovy1444ImgEl = document.getElementById("muscovy-1444-img");
+function showTooltip(element){
+    element.style.display = "flex"
+}
