5f1f23142236f0e4d6e6f1d76b4d0acbe4b2dc4a
Mario Haider <haider.mario@gmx.at>
2023-07-03 15:33:00 +0200

    added image mapping added div-containers and restructured functions to show those elements

--- index.js
+++ index.js
@@ -6,11 +6,36 @@ const denmark1444ImgEl = document.getElementById("denmark-1444-img");
 const england1444ImgEl = document.getElementById("england-1444-img");
 const france1444ImgEl = document.getElementById("france-1444-img");
 const castile1444ImgEl = document.getElementById("castile-1444-img");
-const muscovy1444ImgEl = document.getElementById("muscovy-1444-img");
+const muscovyTooltipEl = document.getElementById("muscovy-tooltip");
+const austriaTooltipEl = document.getElementById("austria-tooltip");
+const ottomanTooltipEl = document.getElementById("ottoman-tooltip");
+const polandTooltipEl = document.getElementById("poland-tooltip");
+const brandenburgTooltipEl = document.getElementById("brandenburg-tooltip");
+const denmarkTooltipEl = document.getElementById("denmark-tooltip");
+const englandTooltipEl = document.getElementById("england-tooltip");
+const franceTooltipEl = document.getElementById("france-tooltip");
+const castileTooltipEl = document.getElementById("castile-tooltip");
+const tooltipContainerEl = document.getElementById("tooltip-container");
 
-function showTooltip(element){
+function showTooltipRight(element){
+    const x = event.clientX;
+    const y = event.clientY;
+
+    tooltipContainerEl.style.display ="grid"
     element.style.display = "grid"
+    tooltipContainerEl.style.left = x + "px";
+    tooltipContainerEl.style.top = y + "px";
 }
 function hideTooltip(element){
+    tooltipContainerEl.style.display = "none"
     element.style.display = "none"
+}
+function showTooltipLeft(element){
+    const x = event.clientX - 400;
+    const y = event.clientY ;
+
+    tooltipContainerEl.style.display ="grid"
+    element.style.display = "grid"
+    tooltipContainerEl.style.left = x + "px";
+    tooltipContainerEl.style.top = y + "px";
 }
